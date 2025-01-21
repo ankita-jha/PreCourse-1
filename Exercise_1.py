44a5c98b99d46ad910260c220bdb5deb46d7aadc
Ankita <ankitajha@Ankitas-MacBook-Air.local>
2025-01-20 19:25:16 -0500

    Done exercise 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,41 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
+  # Space Complexity: O(n)
+  # Time : O(n)
      def __init__(self):
+         self.arr=[]
+         self.top=-1
          
      def isEmpty(self):
+       if len(self.arr)==0:
+         return True
+       return False
          
      def push(self, item):
+       
+       self.arr.append(item)
+       self.top+=1
+       
          
      def pop(self):
+       if self.isEmpty()==True:
+         return "stack underflow"
+       element=self.arr[self.top]
+       self.top-=1
+       return element
         
         
      def peek(self):
+       if self.isEmpty()==True:
+         return "stack underflow"
+       return self.arr[top]
         
      def size(self):
+       return self.top+1
          
      def show(self):
+       return self.arr.copy()
          
 
 s = myStack()
